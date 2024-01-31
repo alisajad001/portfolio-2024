5a476cf6746b33786d11f6a895d8d55e825f2059
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-01-31 15:29:22 +0430

    Add: Website basic components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }

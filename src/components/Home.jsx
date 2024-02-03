b4ea33211ebc3c12c0b64ce2ddc2b74de48675e8
alisajad001 <forwebonly0@gmail.com>
2024-02-03 22:00:46 +0430

    Style: Home/hero section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,25 @@
 function Home() {
-  return <div>Home</div>;
+  return (
+    <main>
+      <section id="home">
+        <div className="container">
+          <h1 className="home_title">
+            Front end developer <br /> designer
+          </h1>
+          <p>
+            I'm a passionate front-end developer. Crafting seamless digital
+            experiences with innovation and design in mind.
+            <br />
+          </p>
+          <button>
+            <a href="#">See Works</a>
+          </button>
+
+          <div className="mouse-animation"></div>
+        </div>
+      </section>
+    </main>
+  );
 }
 
 export default Home;

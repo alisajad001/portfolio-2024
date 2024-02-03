b738a927121da23b522c4fcc2724a0cca2cf2f6f
alisajad001 <forwebonly0@gmail.com>
2024-02-03 22:56:01 +0430

    Style: Home/ Recent projects section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 function Home() {
   return (
     <main>
+      {/* Home start */}
       <section id="home">
         <div className="container">
           <h1 className="home_title">
@@ -11,11 +12,61 @@ function Home() {
             experiences with innovation and design in mind.
             <br />
           </p>
-          <button>
+          <button className="btn-primary">
             <a href="#">See Works</a>
           </button>
 
           <div className="mouse-animation"></div>
+        </div>
+      </section>
+      {/* Home end */}
+
+      <section id="recent-projects">
+        <div className="container">
+          <div className="recent-projects-text">
+            <h2 className="section-title">Recent Projects</h2>
+            <p>Some of my recent projects made with React</p>
+          </div>
+          <div className="projects-container">
+            <div className="project">
+              <img
+                src="https://htmlburger.com/blog/wp-content/uploads/2023/04/modern-website-design-examples.jpg"
+                alt="Recent Project"
+              />
+
+              <div className="project-info">
+                <h3>Weather App React</h3>
+                <p>
+                  A simple Lorem ipsum dolor sit amet consectetur adipisicing
+                  elit. Velit esse maiores consequuntur, deleniti odio
+                  praesentium!
+                </p>
+
+                <a href="#" className="btn-outline">
+                  View Live
+                </a>
+              </div>
+            </div>
+            <div className="project">
+              <img
+                src="https://htmlburger.com/blog/wp-content/uploads/2023/04/modern-website-design-examples.jpg"
+                alt="Recent Project"
+              />
+
+              <div className="project-info">
+                <h3>Weather App React</h3>
+                <p>
+                  A simple Lorem ipsum dolor sit amet consectetur adipisicing
+                  elit. Velit esse maiores consequuntur, deleniti odio
+                  praesentium!
+                </p>
+
+                <a href="#" className="btn-outline">
+                  View Live
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </main>

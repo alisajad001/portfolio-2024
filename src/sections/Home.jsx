6015bb01418588359b2cf8e96e2c0ffff08e4e70
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-03-21 23:12:14 +0430

    Start: Home content

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -1,5 +1,39 @@
+import { useState, useEffect } from 'react';
+
 function Home() {
-  return <div>Home</div>;
+  const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const updateCursorPosition = (event) => {
+      setCursorPosition({ x: event.clientX, y: event.clientY });
+    };
+
+    document.addEventListener('mousemove', updateCursorPosition);
+
+    return () => {
+      document.removeEventListener('mousemove', updateCursorPosition);
+    };
+  }, []);
+
+  const text = 'Front end developer©';
+  const letters = text.split('').map((letter, index) => (
+    <span key={index} className="animated-text">
+      {letter}
+    </span>
+  ));
+
+  return (
+    <main className="container mx-auto flex justify-center items-center h-screen">
+      <div
+        className="custom-cursor"
+        style={{ left: cursorPosition.x, top: cursorPosition.y }}
+      ></div>
+
+      <h1 className="text-4xl sm:text-5xl md:text-7xl lg:text-9xl uppercase font-black text-gray-950 antialiased">
+        {letters.slice(0, 10)} <br /> {letters.slice(10, 20)}
+      </h1>
+    </main>
+  );
 }
 
 export default Home;

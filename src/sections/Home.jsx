18b88ed13e2fa5ad4151f95ff82bc66e0e8ca646
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-03-22 18:39:01 +0430

    Add: Span date

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -24,15 +24,19 @@ function Home() {
   ));
 
   return (
-    <main className="container mx-auto flex flex-col justify-center items-center h-[35rem] 2xl:h-[50rem] relative">
+    <main className="container mx-auto flex flex-col justify-center items-center h-screen 2xl:h-[50rem] relative  overflow-hidden sm:overflow-visible">
       <div
         className="custom-cursor"
         style={{ left: cursorPosition.x, top: cursorPosition.y }}
       ></div>
 
       {/* Gradient bubbles */}
-      <div className="absolute rounded-full top-20 left-0 w-96 h-96 bg-gradient-to-tr from-pink-500 to-yellow-400 mix-blend-multiply filter blur-3xl opacity-70 animate-move"></div>
+      <div className="absolute rounded-full top-20 left-0 w-96 h-96 bg-gradient-to-tr from-pink-500 to-yellow-400 mix-blend-multiply filter blur-3xl opacity-70"></div>
       <div className="absolute rounded-full bottom-0 right-20 w-96 h-96 bg-gradient-to-br from-indigo-500 to-purple-600 mix-blend-multiply filter blur-3xl opacity-50"></div>
+
+      <span className="text-[10px] md:text-sm uppercase text-gray-800 font-bold tracking-widest absolute top-32">
+        Hello World | 2024
+      </span>
 
       <h1 className="text-4xl z-10 sm:text-5xl md:text-7xl lg:text-9xl uppercase font-black text-gray-950 antialiased leading-8">
         {letters.slice(0, 10)} <br /> {letters.slice(10, 20)}

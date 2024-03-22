7b0fdb5d06c5acfcf9ff5bdaf6f1cb071edfca77
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-03-22 21:53:23 +0430

    Start: Footer content

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,5 +1,12 @@
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="pb-3">
+      <div className="w-full max-w-7xl mx-auto rounded-md z-50 flex justify-between items-center p-2 backdrop-blur-md bg-white/30 border border-white/30">
+        <p>Ali Sajad Ahmadi</p>
+        <p className="text-[10px]">2024</p>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;

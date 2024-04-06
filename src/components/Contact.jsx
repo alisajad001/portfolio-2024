81a6ffe7f2f1d29ffa62b559d2c73aac799d0500
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-04-06 18:28:14 +0430

    Update Contact component layout and content

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,7 @@ import Button from './Button';
 
 export default function Contact() {
   return (
-    <section className="container mx-auto p-6 my-10 md:flex items-end justify-between">
+    <section className="container mx-auto px-6 my-10 lg:flex items-end justify-between">
       <h2 className="text-3xl sm:text-5xl md:text-7xl max-w-3xl font-medium text-gray-900">
         Let's talk about your stunning Idea and make it real.
       </h2>

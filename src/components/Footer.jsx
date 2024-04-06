7975968fb5834aaf3fdcdd100795ef56536a45e2
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-04-06 18:28:18 +0430

    Update Footer component layout and styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="container mx-auto p-6 mt-10 text-[12px] text-gray-600">
+    <footer className="container mx-auto px-6 py-8 mt-10 border-t text-sm text-gray-600 md:flex flex-row-reverse justify-between items-center">
       <ul className="flex gap-5">
         <li>
           <a href="#">GitHub</a>
@@ -12,7 +12,7 @@ export default function Footer() {
           <a href="#">Contact</a>
         </li>
       </ul>
-      <p className="mt-3">
+      <p className="mt-3 md:mt-0">
         &copy; 2024 All Rights Reserved, Coded with ❤️️ by Ali Sajad
       </p>
     </footer>

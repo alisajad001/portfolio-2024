04b5ff31c09af7d430dd775edc55ccddddb2f060
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-04-06 17:41:05 +0430

    Refactor About and Contact components to use reusable Button component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,3 +1,5 @@
+import Button from './Button';
+
 export default function About() {
   return (
     <section className="container mx-auto p-6 mt-10">
@@ -5,9 +7,8 @@ export default function About() {
         I am a Front end web developer based in Kabul. I am specialized in
         making websites with modern technologies like React and Tailwind CSS.
       </p>
-      <button className="rounded-full flex justify-between bg-gray-950 py-2 px-5 mt-7 text-white">
-        Contact me
-      </button>
+
+      <Button>Get in touch</Button>
     </section>
   );
 }
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,3 +1,5 @@
+import Button from './Button';
+
 export default function Contact() {
   return (
     <section className="container mx-auto p-6 my-10">
@@ -5,9 +7,8 @@ export default function Contact() {
       <h2 className="text-3xl font-medium text-gray-900">
         Let’s talk about a project, collaboration or an idea you may have
       </h2>
-      <button className="rounded-full flex justify-between bg-gray-950 py-2 px-5 mt-7 text-white">
-        Contact me
-      </button>
+
+      <Button>Send an email</Button>
     </section>
   );
 }

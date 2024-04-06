a8ab44ebaf81e03d110a4a147d5612a887ab2e84
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-04-06 18:23:04 +0430

    Update Home component layout and styles

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,11 @@
 export default function Home() {
   return (
-    <section className="container mx-auto p-6 mt-20">
-      <span className="text-sm block mb-3 text-rose-500">2024</span>
-      <h1 className="font-medium text-5xl text-gray-900">
-        Web <br /> Developer
+    <section className="container mx-auto p-6 mt-20 lg:flex items-end justify-between">
+      <h1 className="text-5xl sm:text-7xl md:text-[9rem] md:mt-20 text-gray-900">
+        Web <br /> Developer.
       </h1>
-      <p className="text-gray-500 font-extralight mt-5">
-        specialized in React JS, Front End Development
+      <p className="text-gray-500 font-extralight mt-5 max-w-44">
+        specialized in React JS, Front End Development.
       </p>
     </section>
   );

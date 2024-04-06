eb2732f963c1a25b2a320ff5e88d041342f7f9fa
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-04-06 18:23:17 +0430

    Update About component content and styles

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,10 +2,12 @@ import Button from './Button';
 
 export default function About() {
   return (
-    <section className="container mx-auto p-6 mt-10">
-      <p className="text-2xl text-gray-900 leading-relaxed">
+    <section className="container mx-auto p-6 my-20">
+      <p className="text-3xl text-gray-900 leading-relaxed max-w-2xl">
         I am a Front end web developer based in Kabul. I am specialized in
         making websites with modern technologies like React and Tailwind CSS.
+        Some of my hobbies are reading books, playing football and watching
+        movies.
       </p>
 
       <Button>Get in touch</Button>

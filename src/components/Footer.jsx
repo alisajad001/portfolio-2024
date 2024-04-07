076428b95a49c5e427c30daeba9519d1ee475c36
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-04-07 09:08:12 +0430

    Fix footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,13 +3,13 @@ export default function Footer() {
     <footer className="container mx-auto px-6 py-8 mt-10 border-t text-sm text-gray-600 md:flex flex-row-reverse justify-between items-center">
       <ul className="flex gap-5">
         <li>
-          <a href="#">GitHub</a>
+          <a href="https://github.com/alisajad001">GitHub</a>
         </li>
         <li>
-          <a href="#">LinkedIn</a>
+          <a href="https://linkedin.com/in/alisajad001">LinkedIn</a>
         </li>
         <li>
-          <a href="#">Contact</a>
+          <a href="https://x.com/alisajad07">X</a>
         </li>
       </ul>
       <p className="mt-3 md:mt-0">

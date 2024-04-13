e27907df04e01d3154f2b6ef67acf2a576b0a2d3
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-04-13 18:15:32 +0430

    Update Button component to include hover effect and handle mouse over event

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,19 @@
 export default function Button({ children }) {
+  // Function to handle mouse over event
+  const handleMouseOver = (e) => {
+    const x = e.pageX - e.target.offsetLeft;
+    const y = e.pageY - e.target.offsetTop;
+
+    e.target.style.setProperty('--x', `${x}px`);
+    e.target.style.setProperty('--y', `${y}px`);
+  };
+
   return (
-    <button className="transition-all duration-500 transform hover:bg-rose-500 rounded-full flex justify-between bg-gray-950 py-2 px-5 mt-7 text-white relative overflow-hidden">
-      {children}
+    <button
+      onMouseOver={handleMouseOver}
+      className="transform rounded-full flex justify-between bg-gray-950 py-2 px-5 mt-7 text-white relative overflow-hidden btn-primary"
+    >
+      <span className="relative z-10">{children}</span>
     </button>
   );
 }
